3e1bc54642e5b65370e45a0b06f0bdd82e3d831f
hoangbinhxs <hoangbinhxs@gmail.com>
2023-03-11 17:28:16 +0100

    add eureka

--- bike-service/src/main/java/com/example/bikeservice/BikeServiceApplication.java
+++ bike-service/src/main/java/com/example/bikeservice/BikeServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.bikeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 public class BikeServiceApplication {
 
 	public static void main(String[] args) {
