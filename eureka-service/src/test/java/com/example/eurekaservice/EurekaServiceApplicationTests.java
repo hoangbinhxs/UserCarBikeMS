316cda8f6871ffc79959c6ce8b7d000b89b78bbc
hoangbinhxs <hoangbinhxs@gmail.com>
2023-03-11 18:03:35 +0100

    fix eureka

--- eureka-service/src/test/java/com/example/eurekaservice/EurekaServiceApplicationTests.java
+++ eureka-service/src/test/java/com/example/eurekaservice/EurekaServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eurekaserver;
+package com.example.eurekaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class EurekaServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
